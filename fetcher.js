104b921048b199cfb9e4ff1a5f3dedbc2f3b020a
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-08-26 21:08:25 -0700

    done

--- fetcher.js
+++ fetcher.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const request = require('request');
 
 const url = process.argv.slice(2)[0];
-const target = process.argv.slice(2)[1];
+const fileTarget = process.argv.slice(2)[1];
 
 //request the page (error, response, body)
 const getBody = callback => {
@@ -12,22 +12,13 @@ const getBody = callback => {
 	});
 };
 
-// const content = getBody(item => console.log(item));
 getBody(content => {
-	fs.writeFile(target, content, 'utf8', error => {
+	fs.writeFile(fileTarget, content, 'utf8', error => {
 		if (error) throw error;
-		const fileSize = fs.statSync(target).size;
-		// console.log(fileSize);
+		const fileSize = fs.statSync(fileTarget).size;
 		console.log(`Downloaded and saved ${fileSize} bytes to ./index.html`);
 	});
 });
-
-// const newFile = fs.writeFile(target, getBody(content => content), 'utf8', error => {
-// 	if (error) throw error;
-// 	const fileSize = fs.statSync(target);
-
-// 	console.log(`Downloaded and saved ${fileSize} bytes to ./index.html`);
-// });
 
 // create file index.html(target file) in the current directory
 // open the file
